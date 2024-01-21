5b9522bdd16f889611fc9fd60d09c493bfcdba46
prabinnnn <prabinbhandari120@gmail.com>
2024-01-21 21:48:03 +0545

    add exp

---
+++ exp.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/:fname/:lname?", (req, res) => {
+  const data = req.params.fname; // Corrected typo here
+  const data1 = req.params.lname;
+  res.json(`name is ${data}${data1}`);
+});
+
+app.listen(7656, () => {
+  console.log("app is running");
+});
