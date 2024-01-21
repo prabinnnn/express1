4d5c5873fd98e79eab00e2b2d7182ed0f29e16c0
prabinnnn <prabinbhandari120@gmail.com>
2024-01-21 22:47:05 +0545

    add exp

--- exp.js
+++ exp.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/:fname/:lname?", (req, res) => {
+app.get("/:fname?lname", (req, res) => {
   const data = req.params.fname; // Corrected typo here
   const data1 = req.params.lname;
   res.json(`name is ${data}${data1}`);
