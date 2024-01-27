86714f69045a9e004e7db543d8a7c23ae94f7d58
prabinnnn <prabinbhandari120@gmail.com>
2024-01-27 19:07:54 +0545

    add index

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const indexodroute = require("../route");
-app.use("{ ");
+app.use(body.params);
 app.use("/", indexodroute);
 app.get(3000, () => {
   console.log("app is runing");
